48d56664dcae68e5a226d3887a859b4b35f3e0e5
Cory Spicer <corys4139@gmail.com>
2020-10-01 23:00:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Breadcrumbs
+
+Personal connections left behind and found.
+
+Built in React.
+
+## Team
+- Cory
+- Margaret
+- Nina
+- Rob
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+import Signup from "../Signup/Signup";
+import Login from "../Login/Login";
+import authService from "../../services/authService";
+import Users from '../Users/Users'
+import "./App.css";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi()
+
+class App extends Component {
+  state = {
+    loggedIn: false,
+    spotifyToken: '',
+    user: authService.getUser()
+  };
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    this.setState({spotifyToken : hashParams.access_token})
+    return hashParams;
+  }
+
+  handleLogout = () => {
+    authService.logout();
+    this.setState({ user: null });
+  };
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: authService.getUser() });
+  };
+
+  async componentDidMount() {
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      this.setState({loggedIn: true})
+      spotifyApi.setAccessToken(token);
+    }
+  }
+
+  render() {
+    const {user} = this.state
+    return (
+      <>
+        <NavBar user={this.state.user} handleLogout={this.handleLogout}/>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <main>
+              <h1>Welcome. This is an authorization template.</h1>
+            </main>
+          )}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={({ history }) => (
+            <Signup
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/login"
+          render={({ history }) => (
+            <Login
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/users"
+          render={() =>
+            user ? <Users /> : <Redirect to="/login" />
+          }
+        />
+      </>
+    );
+  }
+}
+
+export default App;
